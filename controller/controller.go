2956d3051b6034fec21315765303d0a13575a83c
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2023-05-11 20:27:50 +0200

    Initial commit

---
+++ controller/controller.go
@@ -0,0 +1,41 @@
+package controller
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"os"
+	"sr-example/example-service/service"
+)
+
+var router = gin.Default()
+
+func Run() {
+
+	port := os.Getenv("SR_EXAMPLE_PORT")
+
+	if port == "" {
+		fmt.Println("no application port given! Please set SR_EXAMPLE_PORT.")
+		return
+	}
+
+	exampleController()
+
+	router.GET("/actuator", actuator)
+
+	err := router.Run(":" + port)
+	if err != nil {
+		fmt.Println("Unable to start application on port " + port)
+		return
+	}
+}
+
+func actuator(c *gin.Context) {
+
+	state := "OPERATIONAL"
+
+	if !service.PingDatabase() {
+		state = "DATABASE_DISCONNECTED"
+	}
+	c.String(http.StatusOK, state)
+}
---
+++ service/service.go
@@ -0,0 +1,31 @@
+package service
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"os"
+	"time"
+)
+
+var client *mongo.Client
+
+func Init(c *mongo.Client) {
+	database := c.Database(os.Getenv("SR_EXAMPLE_MONGO_DATABASE"))
+	client = c
+
+	exampleService(database)
+}
+
+func PingDatabase() bool {
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*100)
+	defer cancel()
+
+	err := client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		return false
+	}
+
+	return true
+}
