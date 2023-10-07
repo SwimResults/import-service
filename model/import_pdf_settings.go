e95498637e81ccae57867243446c059fa0f3d809
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2023-10-07 15:51:00 +0100

    expand import pdf settings

--- model/import_pdf_settings.go
+++ model/import_pdf_settings.go
@@ -2,7 +2,15 @@ package model
 
 type ImportPdfStartListSettings struct {
 	EventSeparator     string   `json:"event_separator,omitempty" bson:"event_separator,omitempty"`
-	EventIgnoreStrings []string `json:"event_ignore_strings,omitempty" bson:"event_ignore_strings,omitempty"`
+	EventSkipStrings   []string `json:"event_skip_strings,omitempty" bson:"event_skip_strings,omitempty"`
+	EventNoSkipStrings []string `json:"event_no_skip_strings,omitempty" bson:"event_no_skip_strings,omitempty"`
 	HeatSeparator      string   `json:"heat_separator,omitempty" bson:"heat_separator,omitempty"`
-	HeatIgnoreStrings  []string `json:"heat_ignore_strings,omitempty" bson:"heat_ignore_strings,omitempty"`
+	HeatSkipStrings    []string `json:"heat_skip_strings,omitempty" bson:"heat_skip_strings,omitempty"`
+	HeatNoSkipStrings  []string `json:"heat_no_skip_strings,omitempty" bson:"heat_no_skip_strings,omitempty"`
+}
+
+type ImportPdfResultListSettings struct {
+	EventSeparator     string   `json:"event_separator,omitempty" bson:"event_separator,omitempty"`
+	EventSkipStrings   []string `json:"event_skip_strings,omitempty" bson:"event_skip_strings,omitempty"`
+	EventNoSkipStrings []string `json:"event_no_skip_strings,omitempty" bson:"event_no_skip_strings,omitempty"`
 }
