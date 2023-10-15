359f415e8284c4293229683e30ede1028d58c2ab
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2023-10-15 23:19:52 +0200

    some pdf importer changes

--- importer/pdf_importer.go
+++ importer/pdf_importer.go
@@ -3,20 +3,33 @@ package importer
 import (
 	"bytes"
 	"github.com/ledongthuc/pdf"
+	importModel "github.com/swimresults/import-service/model"
 )
 
+// ReadPdf opens pdf under given path and reads plain text to a buffer and returns content as string
 func ReadPdf(path string) (string, error) {
 	f, r, err := pdf.Open(path)
-	// remember close file
 	defer f.Close()
 	if err != nil {
 		return "", err
 	}
+
 	var buf bytes.Buffer
 	b, err := r.GetPlainText()
 	if err != nil {
 		return "", err
 	}
+
 	buf.ReadFrom(b)
 	return buf.String(), nil
+}
+
+// ImportPdfStartListFile takes the path to a pdf file that contains a start
+// list. All events, teams, athletes, heats and starts will be imported.
+// If exclude is set, given event numbers will be excluded from import.
+// If include is set, only given event numbers will be imported.
+//
+// For import process details see documentation on GitHub.
+func ImportPdfStartListFile(file string, meeting string, exclude []int, include []int) (*importModel.ImportFileStats, error) {
+	return nil, nil
 }
