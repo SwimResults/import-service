a4fe54fe3ae3d4c20495b258e45a7dfbfaa94da5
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2023-11-05 22:48:21 +0100

    live meeting json fixes

--- importer/easy_wk_importer.go
+++ importer/easy_wk_importer.go
@@ -16,7 +16,7 @@ func SetEasyWkMeeting() {
 	dat, _ := os.ReadFile("config/live_meeting.json")
 	err := json.Unmarshal(dat, &CurrentMeeting)
 	if err != nil {
-		panic(err)
+		println(err)
 		return
 	}
 	fmt.Printf("set meeting for live services to: '%s'; password is: '%s'\n", CurrentMeeting.Meeting, CurrentMeeting.Password)
