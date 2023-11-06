494ed80ee0d6d87351c4b50ff47e3e111524b71f
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2023-11-06 10:46:37 +0100

    include config in dockerfile

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ FROM golang:1.20.5-alpine3.18
 WORKDIR /app
 
 COPY import-service /app/service
+COPY config /app/config
 RUN chmod +x /app/service
 
 ENV SR_IMPORT_PORT=8080
--- importer/easy_wk_importer.go
+++ importer/easy_wk_importer.go
@@ -15,7 +15,7 @@ var CurrentMeeting model.EasyWkMeeting
 func SetEasyWkMeeting() {
 	wd, _ := os.Getwd()
 	println(wd)
-	dat, err1 := os.ReadFile("../config/live_meeting.json")
+	dat, err1 := os.ReadFile("config/live_meeting.json")
 	if err1 != nil {
 		println(err1.Error())
 		return
