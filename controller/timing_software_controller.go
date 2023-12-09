d6fed65d2aad932cb475fb3fc8b4a47c9faf5c2f
konrad2002 <konrad@schwimmteamerzgebirge.de>
2023-12-09 08:29:18 +0100

    easy wk endpoint for admin panel

--- controller/timing_software_controller.go
+++ controller/timing_software_controller.go
@@ -17,6 +17,10 @@ func timingSoftwareController() {
 	router.POST("/easywk/v2/livework.php", easyWkLivetimingV2)
 	router.GET("/easywk/v2/livework.php", easyWkLivetimingV2)
 	router.OPTIONS("/easywk/v2/livework.php", ok)
+
+	router.POST("/easywk/v3", easyWkLivetimingV3)
+	router.GET("/easywk/v3", easyWkLivetimingV3)
+	router.OPTIONS("/easywk/v3", ok)
 }
 
 func easyWkLivetiming(c *gin.Context) {
@@ -28,6 +32,23 @@ func easyWkLivetiming(c *gin.Context) {
 	c.Request.URL.RawQuery = string(body)
 
 	if err := c.BindQuery(&request); err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
+
+	str, err := service.EasyWkLivetimingRequest(request)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "ERROR: %s", err.Error())
+		return
+	}
+
+	c.String(http.StatusOK, str)
+}
+
+func easyWkLivetimingV3(c *gin.Context) {
+	var request model.EasyWkActionRequest
+
+	if err := c.BindJSON(&request); err != nil {
 		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
 		return
 	}
