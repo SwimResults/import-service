08a1edca62ec4cf41a37c6aa6ed157a39b59bbdf
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2024-04-12 14:53:20 +0200

    use json instead of form in easy_wk_action_request

--- model/easy_wk_action_request.go
+++ model/easy_wk_action_request.go
@@ -1,63 +1,63 @@
 package model
 
 type EasyWkActionRequest struct {
-	Password    string `form:"pwd"`
-	Action      string `form:"action"`
-	KeepSum     string `form:"keepsum,omitempty"`
-	FirstLane   int    `form:"firstlane,omitempty"`
-	LaneCount   int    `form:"lanecount,omitempty"`
-	MeetingName string `form:"vername,omitempty"`
-	Event       int    `form:"event,omitempty"`
-	Heat        int    `form:"heat,omitempty"`
-	MaxHeat     int    `form:"maxheat,omitempty"`
-	EventName   string `form:"name,omitempty"`
-	Lane        int    `form:"lane,omitempty"`
-	Meter       string `form:"meter,omitempty"`
-	Time        int    `form:"time,omitempty"`
-	Finished    string `form:"finished,omitempty"`
-	Content     string `form:"content,omitempty"`
+	Password    string `json:"pwd"`
+	Action      string `json:"action"`
+	KeepSum     string `json:"keepsum,omitempty"`
+	FirstLane   int    `json:"firstlane,omitempty"`
+	LaneCount   int    `json:"lanecount,omitempty"`
+	MeetingName string `json:"vername,omitempty"`
+	Event       int    `json:"event,omitempty"`
+	Heat        int    `json:"heat,omitempty"`
+	MaxHeat     int    `json:"maxheat,omitempty"`
+	EventName   string `json:"name,omitempty"`
+	Lane        int    `json:"lane,omitempty"`
+	Meter       string `json:"meter,omitempty"`
+	Time        int    `json:"time,omitempty"`
+	Finished    string `json:"finished,omitempty"`
+	Content     string `json:"content,omitempty"`
 
-	Athlete0 string `form:"srw0,omitempty"`
-	Year0    string `form:"yob0,omitempty"`
-	Team0    string `form:"club0,omitempty"`
+	Athlete0 string `json:"srw0,omitempty"`
+	Year0    string `json:"yob0,omitempty"`
+	Team0    string `json:"club0,omitempty"`
 
-	Athlete1 string `form:"srw1,omitempty"`
-	Year1    string `form:"yob1,omitempty"`
-	Team1    string `form:"club1,omitempty"`
+	Athlete1 string `json:"srw1,omitempty"`
+	Year1    string `json:"yob1,omitempty"`
+	Team1    string `json:"club1,omitempty"`
 
-	Athlete2 string `form:"srw2,omitempty"`
-	Year2    string `form:"yob2,omitempty"`
-	Team2    string `form:"club2,omitempty"`
+	Athlete2 string `json:"srw2,omitempty"`
+	Year2    string `json:"yob2,omitempty"`
+	Team2    string `json:"club2,omitempty"`
 
-	Athlete3 string `form:"srw3,omitempty"`
-	Year3    string `form:"yob3,omitempty"`
-	Team3    string `form:"club3,omitempty"`
+	Athlete3 string `json:"srw3,omitempty"`
+	Year3    string `json:"yob3,omitempty"`
+	Team3    string `json:"club3,omitempty"`
 
-	Athlete4 string `form:"srw4,omitempty"`
-	Year4    string `form:"yob4,omitempty"`
-	Team4    string `form:"club4,omitempty"`
+	Athlete4 string `json:"srw4,omitempty"`
+	Year4    string `json:"yob4,omitempty"`
+	Team4    string `json:"club4,omitempty"`
 
-	Athlete5 string `form:"srw5,omitempty"`
-	Year5    string `form:"yob5,omitempty"`
-	Team5    string `form:"club5,omitempty"`
+	Athlete5 string `json:"srw5,omitempty"`
+	Year5    string `json:"yob5,omitempty"`
+	Team5    string `json:"club5,omitempty"`
 
-	Athlete6 string `form:"srw6,omitempty"`
-	Year6    string `form:"yob6,omitempty"`
-	Team6    string `form:"club6,omitempty"`
+	Athlete6 string `json:"srw6,omitempty"`
+	Year6    string `json:"yob6,omitempty"`
+	Team6    string `json:"club6,omitempty"`
 
-	Athlete7 string `form:"srw7,omitempty"`
-	Year7    string `form:"yob7,omitempty"`
-	Team7    string `form:"club7,omitempty"`
+	Athlete7 string `json:"srw7,omitempty"`
+	Year7    string `json:"yob7,omitempty"`
+	Team7    string `json:"club7,omitempty"`
 
-	Athlete8 string `form:"srw8,omitempty"`
-	Year8    string `form:"yob8,omitempty"`
-	Team8    string `form:"club8,omitempty"`
+	Athlete8 string `json:"srw8,omitempty"`
+	Year8    string `json:"yob8,omitempty"`
+	Team8    string `json:"club8,omitempty"`
 
-	Athlete9 string `form:"srw9,omitempty"`
-	Year9    string `form:"yob9,omitempty"`
-	Team9    string `form:"club9,omitempty"`
+	Athlete9 string `json:"srw9,omitempty"`
+	Year9    string `json:"yob9,omitempty"`
+	Team9    string `json:"club9,omitempty"`
 
-	Athlete10 string `form:"srw10,omitempty"`
-	Year10    string `form:"yob10,omitempty"`
-	Team10    string `form:"club10,omitempty"`
+	Athlete10 string `json:"srw10,omitempty"`
+	Year10    string `json:"yob10,omitempty"`
+	Team10    string `json:"club10,omitempty"`
 }
