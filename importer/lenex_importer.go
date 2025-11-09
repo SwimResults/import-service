0042be41230b88a419e20b9020bf4d63372120b5
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2025-11-09 23:19:52 +0100

    add lenex as import option; use lenex parser; import events, age groups, athletes and teams

--- importer/lenex_importer.go
+++ importer/lenex_importer.go
@@ -1,1 +1,364 @@
 package importer
+
+import (
+	"encoding/xml"
+	"fmt"
+	"github.com/konrad2002/lenexparser/model/elements"
+	"github.com/konrad2002/lenexparser/model/enums"
+	athleteModel "github.com/swimresults/athlete-service/model"
+	importModel "github.com/swimresults/import-service/model"
+	meetingModel "github.com/swimresults/meeting-service/model"
+	startModel "github.com/swimresults/start-service/model"
+	"io"
+	"strconv"
+)
+
+func ImportLenexFile(file string, meeting string, exclude []int, include []int) (*importModel.ImportFileStats, error) {
+	var stats importModel.ImportFileStats
+
+	buf, err1 := getFileReader(file)
+	if err1 != nil {
+		return nil, err1
+	}
+
+	xmlString, err := io.ReadAll(buf)
+	if err != nil {
+		return nil, err
+	}
+
+	var lenex elements.Lenex
+	err = xml.Unmarshal(xmlString, &lenex)
+	if err != nil {
+		return nil, err
+	}
+
+	meet := lenex.Meets[0]
+
+	eventOrdering := 1
+	meetingYear := meet.EntryStartDate.Year()
+
+	heats := map[int]startModel.Heat{}
+
+	for _, session := range meet.Sessions {
+		for _, event := range session.Events {
+
+			// EVENT IMPORT
+			fmt.Printf("%d", event.Number)
+			if !IsEventImportable(event.Number, exclude, include) {
+				print(" => no import")
+				continue
+			}
+
+			println("event import")
+
+			importEvent := meetingModel.Event{
+				Number:   event.Number,
+				Distance: event.SwimStyle.Distance,
+				Meeting:  meeting,
+				Ordering: eventOrdering,
+			}
+
+			eventOrdering++
+
+			switch event.Gender {
+			case enums.EventGenderFemale:
+				importEvent.Gender = "FEMALE"
+				break
+			case enums.EventGenderMale:
+				importEvent.Gender = "MALE"
+				break
+			default:
+				importEvent.Gender = "MIXED"
+			}
+
+			if event.SwimStyle.RelayCount > 1 {
+				importEvent.RelayDistance = fmt.Sprintf("%dx%d", event.SwimStyle.RelayCount, event.SwimStyle.Distance)
+				importEvent.Distance = event.SwimStyle.RelayCount * event.SwimStyle.Distance
+			}
+
+			newEvent, created, err3 := ec.ImportEvent(importEvent, string(event.SwimStyle.Stroke), session.Number)
+			if err3 != nil {
+				return nil, err3
+			}
+
+			if created {
+				stats.Created.Events++
+				print("(+) ")
+			} else {
+				print("( ) ")
+			}
+			println(newEvent.Number)
+
+			stats.Imported.Events++
+
+			// AGE GROUP IMPORT
+			for _, ageGroup := range event.AgeGroups {
+				minAge := meetingYear - ageGroup.AgeMin
+				maxAge := meetingYear - ageGroup.AgeMax
+
+				importAgeGroup := meetingModel.AgeGroup{
+					Meeting: meeting,
+					Event:   event.Number,
+					Default: false,
+					MinAge:  strconv.Itoa(minAge),
+					MaxAge:  strconv.Itoa(maxAge),
+					IsYear:  true,
+					Name:    ageGroup.Name,
+				}
+
+				switch ageGroup.Gender {
+				case enums.AgeGroupGenderFemale:
+					importEvent.Gender = "FEMALE"
+					break
+				case enums.AgeGroupGenderMale:
+					importEvent.Gender = "MALE"
+					break
+				default:
+					importEvent.Gender = "MIXED"
+				}
+
+				newAgeGroup, created, err5 := gc.ImportAgeGroup(importAgeGroup)
+				if err5 != nil {
+					return nil, err5
+				}
+
+				if created {
+					stats.Created.AgeGroups++
+					print("(+) ")
+				} else {
+					print("( ) ")
+				}
+				println(newAgeGroup)
+
+				stats.Imported.AgeGroups++
+			}
+
+			// HEATS
+			for _, heat := range event.Heats {
+				heats[heat.HeatId] = startModel.Heat{
+					Meeting:         meeting,
+					Event:           event.Number,
+					Number:          heat.Number,
+					StartEstimation: heat.Daytime.Time,
+				}
+				// TODO: import heats
+			}
+		}
+	}
+
+	// TEAMS
+	for _, team := range meet.Clubs {
+		stateId, err := strconv.Atoi(team.Region)
+		if err != nil {
+			stateId = 0
+		}
+
+		dsv, err := strconv.Atoi(team.Code)
+		if err != nil {
+			dsv = 0
+		}
+
+		importTeam := athleteModel.Team{ // TODO: check if Contact, Address and Website overwrites
+			Name:    team.Name,
+			Country: string(team.Nation),
+			DsvId:   dsv,
+			StateId: stateId,
+			Contact: athleteModel.Contact{
+				Name:  team.Contact.Name,
+				EMail: team.Contact.Email,
+				Phone: team.Contact.Phone,
+				Fax:   team.Contact.Fax,
+			},
+			Address: athleteModel.Address{
+				Street:     team.Contact.Street,
+				Number:     team.Contact.Street2,
+				City:       team.Contact.City,
+				PostalCode: team.Contact.Zip,
+			},
+			Website: team.Contact.Internet,
+		}
+
+		newTeam, created, err := tc.ImportTeam(importTeam, meeting)
+		if err != nil {
+			return &stats, err
+		}
+		cs := 'o'
+		if created {
+			cs = '+'
+			stats.Created.Teams++
+		}
+		stats.Imported.Teams++
+		fmt.Printf("[ %c ] > id: %s, name: %s, part: %s\n", cs, newTeam.Identifier.String(), newTeam.Name, newTeam.Participation)
+
+		// ATHLETES
+		for _, athlete := range team.Athletes {
+			dsvAthlete, err := strconv.Atoi(athlete.License)
+			if err != nil {
+				dsvAthlete = 0
+			}
+
+			importAthlete := athleteModel.Athlete{
+				Name:      athlete.Firstname + " " + athlete.Lastname,
+				Firstname: athlete.Firstname,
+				Lastname:  athlete.Lastname,
+				Year:      athlete.Birthdate.Year(),
+				DsvId:     dsvAthlete,
+				Team: athleteModel.Team{
+					Identifier: newTeam.Identifier, // TODO: check if import with team id exists
+				},
+			}
+
+			switch athlete.Gender {
+			case enums.GenderFemale:
+				importAthlete.Gender = "FEMALE"
+				break
+			case enums.GenderMale:
+				importAthlete.Gender = "MALE"
+				break
+			default:
+				importAthlete.Gender = "MIXED"
+			}
+
+			newAthlete, created, err := ac.ImportAthlete(importAthlete, meeting)
+			if err != nil {
+				return &stats, err
+			}
+			cs := 'o'
+			if created {
+				cs = '+'
+				stats.Created.Athletes++
+			}
+			stats.Imported.Athletes++
+			fmt.Printf("[ %c ] > id: %s, name: %s, part: %s\n", cs, newAthlete.Identifier.String(), newAthlete.Name, newAthlete.Participation)
+
+			// STARTS + RESULTS + DISQUALIFICATIONS
+			for _, result := range athlete.Results {
+				heat := heats[result.HeatId]
+
+				// START
+				start := startModel.Start{
+					Meeting:         meeting,
+					Event:           heat.Event,
+					HeatNumber:      heat.Number,
+					Lane:            result.Lane,
+					Athlete:         newAthlete.Identifier,
+					AthleteName:     athlete.Firstname + " " + athlete.Lastname,
+					AthleteYear:     athlete.Birthdate.Year(),
+					AthleteTeam:     newAthlete.Team.Identifier,
+					AthleteTeamName: team.Name,
+					Rank:            0, // TODO: get ranks before
+					Certified:       true,
+				}
+				newStart, c, err2 := sc.ImportStart(start)
+				if err2 != nil {
+					return &stats, err2
+				}
+				if c {
+					stats.Created.Starts++
+					fmt.Printf("[ ! ] start has been created: id: '%s'; event: '%d', athlete: '%s'", newStart.Identifier, newStart.Event, newStart.AthleteName)
+				}
+				stats.Imported.Starts++
+			}
+		}
+	}
+
+	fmt.Printf(" +==============================+ \n")
+
+	//var starts []startModel.Start
+	//
+	//for _, dsvResult := range erg.PNErgebnisse {
+	//
+	//	if dsvResult.GrundDerNichtwertung == "AB" {
+	//		continue
+	//	}
+	//
+	//	if unusedEvents[dsvResult.Wettkampfnummer] {
+	//		continue
+	//	}
+	//
+	//	if !IsEventImportable(dsvResult.Wettkampfnummer, exclude, include) {
+	//		continue
+	//	}
+	//
+	//	// RESULT
+	//	result := startModel.Result{
+	//		Time:       dsvResult.Endzeit.Duration(),
+	//		ResultType: "result_list",
+	//	}
+	//
+	//	starts = append(starts, *newStart)
+	//
+	//	if dsvResult.GrundDerNichtwertung != "" {
+	//		disqType := "disqualified"
+	//		switch dsvResult.GrundDerNichtwertung {
+	//		case "NA":
+	//			disqType = "dns"
+	//			break
+	//		case "AU":
+	//			disqType = "dnf"
+	//			break
+	//		case "ZU":
+	//			disqType = "time"
+	//			break
+	//		}
+	//		disqualification, created, err4 := dc.ImportDisqualification(start, dsvResult.Disqualifikationsbemerkung, disqType, time.UnixMicro(0))
+	//		if err4 != nil {
+	//			return &stats, err4
+	//		}
+	//		cs := 'o'
+	//		if created {
+	//			cs = '+'
+	//			stats.Created.Disqualifications++
+	//		}
+	//		stats.Imported.Disqualifications++
+	//		fmt.Printf("[ %c ] > id: %s, type: %s, reason: %s\n", cs, disqualification.Identifier, disqualification.Type, disqualification.Reason)
+	//	} else {
+	//		_, _, err3 := sc.ImportResult(start, result)
+	//		if err3 != nil {
+	//			return &stats, err3
+	//		}
+	//		stats.Created.Results++
+	//		stats.Imported.Results++
+	//	}
+	//
+	//}
+	//
+	//for _, dsvLap := range erg.PNZwischenzeiten {
+	//	if unusedEvents[dsvLap.Wettkampfnummer] {
+	//		continue
+	//	}
+	//
+	//	if !IsEventImportable(dsvLap.Wettkampfnummer, exclude, include) {
+	//		continue
+	//	}
+	//
+	//	// LAP Result
+	//	lapResult := startModel.Result{
+	//		Time:       dsvLap.Zwischenzeit.Duration(),
+	//		ResultType: "lap",
+	//		LapMeters:  dsvLap.Distanz,
+	//	}
+	//
+	//	var lapStart startModel.Start
+	//
+	//	found := false
+	//	for _, start := range starts {
+	//		if start.AthleteMeetingId == dsvLap.VeranstaltungsIdSchwimmer && start.Event == dsvLap.Wettkampfnummer {
+	//			lapStart = start
+	//			found = true
+	//			break
+	//		}
+	//	}
+	//
+	//	if found {
+	//		_, _, err3 := sc.ImportResult(lapStart, lapResult)
+	//		if err3 != nil {
+	//			return &stats, err3
+	//		}
+	//		stats.Created.Results++
+	//		stats.Imported.Results++
+	//	}
+	//}
+
+	return &stats, nil
+}
--- service/import_file_service.go
+++ service/import_file_service.go
@@ -19,6 +19,9 @@ func ImportFile(r model.ImportFileRequest) error {
 			return fmt.Errorf("unknown file_type for DSV (%s)", r.FileType)
 		}
 		return nil
+	case "LEF":
+		go LenexImport(r)
+		return nil
 	case "PDF":
 		switch r.FileType {
 		case "START_LIST":
@@ -54,6 +57,14 @@ func DsvDefinitionImport(r model.ImportFileRequest) {
 
 func DsvResultListImport(r model.ImportFileRequest) {
 	stats, err := importer.ImportDsvResultFile(r.Url, r.Meeting, r.ExcludeEvents, r.IncludeEvents)
+	if err != nil {
+		println(err.Error())
+	}
+	stats.PrintReport()
+}
+
+func LenexImport(r model.ImportFileRequest) {
+	stats, err := importer.ImportLenexFile(r.Url, r.Meeting, r.ExcludeEvents, r.IncludeEvents)
 	if err != nil {
 		println(err.Error())
 	}
